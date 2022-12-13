a3d672404ded583bcceba7ff40318385bba120b9
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-14 02:39:25 +0530

    file

--- my-app/src/Components/DisplayProductMainData.jsx
+++ my-app/src/Components/DisplayProductMainData.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import "./ProductHeadings.css"
+import Carousel from "better-react-carousel";
+import { Link } from 'react-router-dom'
 
 
 
@@ -11,25 +13,51 @@ const DisplayProductMainData = ({src , name , weight ,price , des ,id})=>{
     let style = {
       position:"relative" , 
     }
-
+ const [value , setValue] = useState(false)
     
 
     const imageData = [
       {id:1 , src:"https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6457/6457397_sd.jpg;maxHeight=200;maxWidth=300"},{id:2 , src:"https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6503/6503094_sd.jpg;maxHeight=200;maxWidth=300"},{id:3 , src:"https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6386/6386391_sd.jpg;maxHeight=200;maxWidth=300"},
     ]
 
+    const handleChange = ()=>{
+      setValue(1500)
+      
+    }
+   useEffect(()=>{
+    const id = setTimeout(()=>{
+      setValue(false)
+    },6000)
+    return ()=>{
+      clearTimeout(id)
+    }
+   },[value])
+
+  //  const handleClose = ()=>{
+  //   setValue(false)
+  //  }
       
  return (
     <Box shadow="base" className='hoverProductDiv' w={{base:"90vw", sm: "55vw", md: "43.5vw", lg: "25vw" ,xl: "25vw",'2xl': "23vw",}} h={{base:"434px", sm: "65vh", md: "74vh", lg: "415px" ,xl: "459px",'2xl': "459px",}} style={style}  >
       
-       <div id='productDataImages' style={{ height:"auto"}} >
-        <img src={src} alt={name}/>
+       <div id='productDataImages' style={{ height:"auto"}}  onMouseOver={handleChange} >
+        {/* <img src={src} alt={name}/> */}
 
-            <Box style={{display:"flex" , justifyContent:"space-evenly"}}>
+        <Carousel loop={true} showDots={true}  autoplay={value}>
+          {imageData.map((img) => (
+            <Carousel.Item key={img.id}>
+              <Link>
+                <Img w="full" src={img.src} />
+              </Link>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+
+            {/* <Box style={{display:"flex" , justifyContent:"space-evenly"}}>
             {imageData.map((item)=>{
               return (<img key={item.id} src={item.src} alt="hello" width="40px" height="40px"  />)
             })}
-            </Box>
+            </Box> */}
        </div>
        
        <div id='productDataDesc' style={{border:"2px  #EBECEE" ,height:"35%" , padding:"10px 10px 10px 10px"}}>
