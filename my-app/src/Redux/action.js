1bc62983f70a7b2374a7ad486d243e80eea0b1c9
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-15 00:54:20 +0530

    file

--- my-app/src/Redux/action.js
+++ my-app/src/Redux/action.js
@@ -1,4 +1,13 @@
 
-import {GETPRODUCTLAPTOPDATA_REQUEST ,GETPRODUCTLAPTOPDATA_SUCCESS,GETPRODUCTLAPTOPDATA_FAILURE} from "./actionType"  ;
+import {GETPRODUCTLAPTOPDATA_REQUEST ,GETPRODUCTLAPTOPDATA_SUCCESS,GETPRODUCTLAPTOPDATA_FAILURE} from "./actionType"  ; 
+import axios from "axios"
 
 
+const getDataProduct =()=> (dispatch)=>{
+        dispatch(GETPRODUCTLAPTOPDATA_REQUEST()) 
+        return axios.get(` http://localhost:8080/Laptop`)
+        .then((res)=> dispatch(GETPRODUCTLAPTOPDATA_SUCCESS(res.data))) 
+        .catch(()=> GETPRODUCTLAPTOPDATA_FAILURE())
+}
+
+export {getDataProduct}
