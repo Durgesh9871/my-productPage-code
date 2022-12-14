1daaa9da05bd7c729441367a1ec0c39aad63cef3
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-15 01:09:16 +0530

    file

--- my-app/src/Redux/reducer.js
+++ my-app/src/Redux/reducer.js
@@ -2,15 +2,22 @@ import {GETPRODUCTLAPTOPDATA_REQUEST ,GETPRODUCTLAPTOPDATA_SUCCESS,GETPRODUCTLAP
 
 
 const initialState = {
-    isLaptopError: false ,
     isLaptopLoading: false ,
     productArrayLaptop :[] ,
+    isLaptopError: false ,
 }
 
 
 const LaptopReducer = (oldState=initialState, action)=>{
    switch(action.type){
-
+    case GETPRODUCTLAPTOPDATA_REQUEST:
+        return{...oldState ,isLaptopLoading:true }
+    case GETPRODUCTLAPTOPDATA_SUCCESS:
+        return{...oldState , productArrayLaptop:action.payload , isLaptopError:false} 
+    case GETPRODUCTLAPTOPDATA_FAILURE:
+        return{...oldState , isLaptopError:true , isLaptopLoading:false }  
+       default:
+        return oldState ; 
    }
 
 }
