d45b670291b240d2fb0b797c412d854ba7861cd9
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-16 18:39:22 +0530

    file

--- my-app/src/ComponentsSinglePage/ImageData.jsx
+++ my-app/src/ComponentsSinglePage/ImageData.jsx
@@ -1,16 +1,26 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Image} from '@chakra-ui/react'
 import React from 'react'
 
 const ImageData = () => {
   return (
-    <Box className="ImageMainBox" style={{display:"flex"}} >
+    <Box className="ImageMainBox" style={{display:"flex" , flexDirection:"column" , justifyContent:"center" , alignItems:"center"}} >
          
-         <Box style={{border:"1px solid black" , height:"40vh" , width:"20%"}}>
+        
+         <Box  style={{border:"1px solid blue" ,height:"60vh" , width:"76%" ,display:"flex"  ,justifyContent:"center" , alignItems:"center" }}>
+            <img src="https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70"  alt="image"    />
+         </Box>
 
-         </Box>
-         <Box  style={{border:"1px solid blue" ,height:"70vh" , width:"76%" ,display:"flex"  ,justifyContent:"center" , alignItems:"center"}}>
-            <img src="https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70" alt="image" />
-         </Box>
+         {/*  Diffrent images -------------- */}
+         <Box style={{border:"1px  black" , height:"10vh" , width:"60%" , display:"flex" ,justifyContent:"center"}}>
+            <Box border="1px solid grey">
+                <Image src="https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70" alt="image"  width="100%" height="100%" /></Box>
+            <Box border="1px solid grey" >
+            <Image src="https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70" alt="image"  width="100%" height="100%" />
+            </Box>
+            <Box border="1px solid grey" >
+            <Image src="https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70" alt="image"  width="100%" height="100%" />
+            </Box>
+        </Box>
 
     </Box>
   )
--- my-app/src/Pages/SinglePageProduct.jsx
+++ my-app/src/Pages/SinglePageProduct.jsx
@@ -5,18 +5,18 @@ import { ImageData } from '../ComponentsSinglePage/ImageData'
 const SinglePageProduct = () => {
   return (
     <Box className="SingleProductMainDiv" background="#f1f3f6"  style={{border:"2px  green" , height:"200vh" ,}}>
-        <hr className='horizatalRule' style={{marginBottom: "10px"}} />
+        {/* <hr className='horizatalRule' style={{marginBottom: "10px"}} /> */}
         {/* <br /> */}
         {/*  Main Box of Single Page ------------------------------------------------ */}
         <Box className='SingleProductMain' shadow="2xl"  mt="10px" background='#ffffff' style={{border:"1px solid red" , height:"200vh"  , width:"90%" , margin:"auto" , display:"flex" ,justifyContent:"space-between"  }}>
 
         {/* Image Data Main box ---------------------------------------------------- */}
-        <Box className='SingleProductMainImage' width="40%" style={{border:"1px solid  yellow" , height:"200vh"}}>
+        <Box className='SingleProductMainImage' mt={5} width="40%" style={{border:"1px solid  yellow" , height:"200vh"}}>
         <ImageData />
         </Box>
 
         {/* Single Page Info Main Div ----------------------------------------------------- */}
-        <Box className='SingleProductMainData' width="58%" style={{border:"1px solid  grey" ,height:"200vh"}}>
+        <Box className='SingleProductMainData' mt={5} width="58%" style={{border:"1px solid  grey" ,height:"200vh"}}>
 
         </Box>
         </Box>
