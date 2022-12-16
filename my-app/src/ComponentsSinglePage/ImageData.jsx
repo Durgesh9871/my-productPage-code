ccb1efb787039722708eb5060ba093b1cd386ce4
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-17 00:04:49 +0530

    file

--- my-app/src/ComponentsSinglePage/ImageData.jsx
+++ my-app/src/ComponentsSinglePage/ImageData.jsx
@@ -1,16 +1,33 @@
 import { Box, Image} from '@chakra-ui/react'
 import React from 'react'
+import ReactImageMagnify from 'react-image-magnify'
 
 const ImageData = () => {
   return (
     <Box className="ImageMainBox" style={{display:"flex" , flexDirection:"column" , justifyContent:"center" , alignItems:"center"}} >
          
         
-         <Box  style={{border:"1px solid blue" ,height:"60vh" , width:"76%" ,display:"flex"  ,justifyContent:"center" , alignItems:"center" }}>
-            <img src="https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70"  alt="image"    />
+        
+         <Box  style={{border:"1px solid blue" ,height:"60vh" , width:"76%" ,display:"flex"  ,justifyContent:"center" , alignItems:"center"  }}>
+            
+         <ReactImageMagnify {...{
+    smallImage: {
+        alt: 'Wristwatch by Ted Baker London',
+        isFluidWidth: true,
+        src: "https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70"  ,
+        
+    },
+    largeImage: {
+        src: "https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70",
+        width: 1200,
+        height: 600
+    }
+}} />
+
+
          </Box>
 
-         {/*  Diffrent images -------------- */}
+       {/*  diff images three----------------------------- */}
          <Box style={{border:"1px  black" , height:"10vh" , width:"60%" , display:"flex" ,justifyContent:"center"}}>
             <Box border="1px solid grey">
                 <Image src="https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70" alt="image"  width="100%" height="100%" /></Box>
