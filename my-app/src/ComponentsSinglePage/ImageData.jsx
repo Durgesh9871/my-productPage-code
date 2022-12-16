550ba77fe97f534cbf3f8570054301f3ca18979c
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-17 01:55:59 +0530

    file

--- my-app/src/ComponentsSinglePage/ImageData.jsx
+++ my-app/src/ComponentsSinglePage/ImageData.jsx
@@ -1,6 +1,7 @@
 import { Box, Image} from '@chakra-ui/react'
 import React,{useState} from 'react'
-import ReactImageMagnify from 'react-image-magnify'
+import ReactImageMagnify from 'react-image-magnify' 
+import "./ImageData.css"
 
 const ImageData = () => {
     const [src , setSrc ] = useState("https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6510/6510531_sd.jpg;maxHeight=640;maxWidth=550")
@@ -46,12 +47,12 @@ const ImageData = () => {
 
        {/*  diff images three----------------------------- */}
          <Box style={{border:"1px  black" , height:"10vh" , width:"60%" , display:"flex" ,justifyContent:"center", marginTop:"20px"}}>
-            <Box border="1px solid grey" >
+            <Box border="1px solid #eef0f3" width="30%" className='ThreeImagesChange' padding="7px" >
                 <Image onMouseOver={handleChange} src="https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70" alt="image"  width="100%" height="100%" /></Box>
-            <Box border="1px solid grey"  >
+            <Box border="1px solid #eef0f3" width="30%" className='ThreeImagesChange' padding="7px"  >
             <Image onMouseOver={handleChange} src="https://rukminim1.flixcart.com/image/312/312/xif0q/computer/q/j/c/-original-imagh3wecapg5gqn.jpeg?q=70" alt="image"  width="100%" height="100%" />
             </Box>
-            <Box border="1px solid grey" >
+            <Box border="1px solid #eef0f3" width="30%" className='ThreeImagesChange' padding="7px" >
             <Image onMouseOver={handleChange} src="https://rukminim1.flixcart.com/image/312/312/ksyz8280/computer/8/v/i/cb-11igl05-thin-and-light-laptop-lenovo-original-imag6fh8sqpuk3ts.jpeg?q=70" alt="image"  width="100%" height="100%" />
             </Box>
         </Box>
