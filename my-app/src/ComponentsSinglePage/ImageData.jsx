7b7ed9b11389518da42729edeb2dacec41373685
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-17 10:14:41 +0530

    file

--- my-app/src/ComponentsSinglePage/ImageData.jsx
+++ my-app/src/ComponentsSinglePage/ImageData.jsx
@@ -50,12 +50,13 @@ const ImageData = () => {
        {/*  diff images three----------------------------- */}
          <Box style={{ height:"10vh" , width:"60%" , display:"flex" ,justifyContent:"center", marginTop:"20px"}}>
             <Box border="1px solid #eef0f3" width="30%" className='ThreeImagesChange' padding="7px" >
-                <Image onMouseOver={handleChange} src="https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70" alt="image"  width="100%" height="100%" /></Box>
+                <Image onMouseOver={handleChange} cursor="pointer"
+                  src="https://rukminim1.flixcart.com/image/416/416/kp2y2kw0/computer/y/0/c/na-thin-and-light-laptop-asus-original-imag3ebnzawky4kn.jpeg?q=70" alt="image"  width="100%" height="100%" /></Box>
             <Box border="1px solid #eef0f3" width="30%" className='ThreeImagesChange' padding="7px"  >
-            <Image onMouseOver={handleChange} src="https://rukminim1.flixcart.com/image/312/312/xif0q/computer/q/j/c/-original-imagh3wecapg5gqn.jpeg?q=70" alt="image"  width="100%" height="100%" />
+            <Image onMouseOver={handleChange} cursor="pointer" src="https://rukminim1.flixcart.com/image/312/312/xif0q/computer/q/j/c/-original-imagh3wecapg5gqn.jpeg?q=70" alt="image"  width="100%" height="100%" />
             </Box>
             <Box border="1px solid #eef0f3" width="30%" className='ThreeImagesChange' padding="7px" >
-            <Image onMouseOver={handleChange} src="https://rukminim1.flixcart.com/image/312/312/ksyz8280/computer/8/v/i/cb-11igl05-thin-and-light-laptop-lenovo-original-imag6fh8sqpuk3ts.jpeg?q=70" alt="image"  width="100%" height="100%" />
+            <Image onMouseOver={handleChange} cursor="pointer" src="https://rukminim1.flixcart.com/image/312/312/ksyz8280/computer/8/v/i/cb-11igl05-thin-and-light-laptop-lenovo-original-imag6fh8sqpuk3ts.jpeg?q=70" alt="image"  width="100%" height="100%" />
             </Box>
         </Box>
 
