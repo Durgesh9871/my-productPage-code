05186001849bd45a5d4d342fc0ef1636b109da7c
Durgesh9871 <devisingh9871686098@gmail.com>
2022-12-18 06:31:05 +0530

    file

--- my-app/src/Pages/SinglePageProduct.jsx
+++ my-app/src/Pages/SinglePageProduct.jsx
@@ -20,11 +20,23 @@ const SinglePageProduct = () => {
 }
   
 const handleChangeAdd = ()=>{
-  changeNum(num+1)
+  if(num == 20){
+    changeNum(1)
+  }
+  else{
+
+    changeNum(num+1)
+  }
 }
 
 const handleChangeRemove= ()=>{
-changeNum(num-1)
+  if(num == 1){
+    changeNum(20)
+  }
+  else{
+
+    changeNum(num-1)
+  }
 }
 
 useEffect(()=>{
@@ -36,24 +48,24 @@ GetSinglePageData(num)
      
 
   return (
-    <Box className="SingleProductMainDiv" background="#f1f3f6"  style={{border:"2px solid  green" , height:"200vh" , }}>
+    <Box className="SingleProductMainDiv" background="#f1f3f6"  style={{border:"1px   green" , height:"auto" , }}>
       {/* Button -----left----------------------- */}
         <ChevronLeftIcon onClick={handleChangeRemove} position="absolute" left="-4px" cursor="pointer" top="250px" w={{base:'32px',sm: "45px", md: "50px", lg: "50px" ,xl: "66px",'2xl': "66px",}} h={{base:'32px',sm: "45px", md: "50px", lg: "50px" ,xl: "66px",'2xl': "66px",}}/> 
 
         {/* BUTTON RIGHT-------------------->----------- */}
-     <ChevronRightIcon onClick={handleChangeAdd} position="absolute" cursor="pointer" right="4px" top="250px" w={{base:'32px',sm: "45px", md: "50px", lg: "50px" ,xl: "66px",'2xl': "66px",}} h={{base:'32px',sm: "45px", md: "50px", lg: "50px" ,xl: "66px",'2xl': "66px",}} ml={{base:"-16px"}}/> 
+     <ChevronRightIcon onClick={handleChangeAdd}   position="absolute" cursor="pointer" right="4px" top="250px" w={{base:'32px',sm: "45px", md: "50px", lg: "50px" ,xl: "66px",'2xl': "66px",}} h={{base:'32px',sm: "45px", md: "50px", lg: "50px" ,xl: "66px",'2xl': "66px",}} ml={{base:"-16px"}}/> 
 
 
         {/*  Main Box of Single Page ------------------------------------------------ */}
-        <Box className='SingleProductMain' flexDirection={{base:"column", sm: "column", md: "row", lg: "row",xl: "row",'2xl': "row"}} shadow="2xl"  mt="10px" pb="20px" background='#ffffff' style={{border:"1px solid red" , height:"auto"  , width:"90%" , margin:"auto" , display:"flex" ,justifyContent:"space-between"}}>
+        <Box className='SingleProductMain' flexDirection={{base:"column", sm: "column", md: "row", lg: "row",xl: "row",'2xl': "row"}} shadow="2xl"  mt="10px" pb="20px" background='#ffffff' style={{border:"1px  red" , height:"auto"  , width:"90%" , margin:"auto" , display:"flex" ,justifyContent:"space-between"}}>
 
         {/* Image Data Main box ---------------------------------------------------- */}
-        <Box className='SingleProductMainImage' mt={5}  width={{base:"96%", sm: "62vw", md: "50vw", lg: "42vw",xl: "40vw",'2xl': "40vw"}} style={{border:"1px solid  yellow" , height:"auto"}}>
+        <Box className='SingleProductMainImage' mt={5}  width={{base:"96%", sm: "62vw", md: "50vw", lg: "42vw",xl: "40vw",'2xl': "40vw"}} style={{border:"1px   yellow" , height:"auto"}}>
         <ImageData  allImageData={singleData?.Images && singleData?.Images[0].imageFront} image={singleData?.Images} />
         </Box>
 
         {/* Single Page Info Main Div ----------------------------------------------------- */}
-        <Box className='SingleProductMainData' mt={5}  width={{base:"none", sm: "100%", md: "50vw", lg: "53vw",xl: "55vw",'2xl': "55vw"}} style={{border:"1px solid  grey" ,height:"auto"}}>
+        <Box className='SingleProductMainData' mt={5}  width={{base:"none", sm: "100%", md: "50vw", lg: "53vw",xl: "55vw",'2xl': "55vw"}} style={{border:"1px   grey" ,height:"auto"}}>
          <ProductLaptop item={singleData} />
         </Box>
         </Box>
