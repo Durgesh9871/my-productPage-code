877b7eb4b22a22560d1685bfd7aa7f7f47e03dd4
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-18 08:57:20 +0530

    file

--- my-app/src/ComponentsSinglePage/recentBought.jsx
+++ my-app/src/ComponentsSinglePage/recentBought.jsx
@@ -6,7 +6,7 @@ import "react-multi-carousel/lib/styles.css";
 
 
 const RecentBought = () => {
-  const [data , setData] = useState([])
+ 
 
   const responsive = {
     desktop: {
@@ -36,18 +36,48 @@ const RecentBought = () => {
     }
   };
 
+   //  For getting the data from api of recent bought   
+  const getData = [
+    {  
+    mainImage:"https://rukminim1.flixcart.com/image/832/832/xif0q/track-pant/w/g/n/36-rjo0086-red-tape-original-imaghxbg9hu5vdqu.jpeg?q=70" 
+    } ,
+    {
+    "mainImage":"https://rukminim1.flixcart.com/image/832/832/l0igvww0/shoe/j/x/z/7-brd-678-7-birde-blue-original-imagca7bcj6p99gw.jpeg?q=70"
+    } ,
+    { 
+    
+    mainImage:"https://rukminim1.flixcart.com/image/832/832/xif0q/shoe/p/c/5/-original-imaghrbyty4ggzcb.jpeg?q=70"
+    } ,
+    {
+    mainImage:"https://rukminim1.flixcart.com/image/832/832/xif0q/shoe/c/k/k/gm1251-43-good-minar-black-original-imaft34ej4zx2z63-bb.jpeg?q=70"
+    } ,
+    {
+      mainImage:"https://rukminim1.flixcart.com/image/832/832/xif0q/shoe/j/h/9/10-rso246-red-tape-grey-original-imagk5jyfezgkvqm.jpeg?q=70"
+    } ,
+     
+    
+    {
+    mainImage:"https://rukminim1.flixcart.com/image/832/832/l0pm3680/sock/z/6/i/free-3-ad-203-adidas-original-imagcfzzeseawymf.jpeg?q=70"
+    },
+    {
+    
+    mainImage:"https://rukminim1.flixcart.com/image/832/832/kh2b4i80-0/sock/w/7/g/free-sm-htb-navy-sport-original-imafx5yzm8uj48yv.jpeg?q=70"
+    } ,
+    {
+    mainImage:"https://rukminim1.flixcart.com/image/128/128/l3ahpjk0/track-pant/l/e/o/l-av104stripe-navy-l-grey-avolt-original-imagegfcgjvggzgf.jpeg?q=70"
+    } ,
+    {
+    
+    mainImage:"https://rukminim1.flixcart.com/image/832/832/km57hjk0/track-pant/g/z/9/l-trackpant9015-vitaan-original-imagf4dhrzfenqgt.jpeg?q=70"
+    } ,
+    {
+    mainImage:"https://rukminim1.flixcart.com/image/832/832/kl9rssw0/track-pant/p/u/f/l-bml-202-beko-original-imagyfnvc9vgta6p.jpeg?q=70"
+    } 
+  ]
 
-  //  For getting the data from api of recent bought  
-  const getData = ()=>{
-     axios.get('https://long-pear-giraffe-gown.cyclic.app/api/peopleAlsoBoughtMens')
-     .then((res)=>setData(res.data))
-     .catch((err)=>alert(`Please refresh the page ${err}`))
-    }
 
-
-useEffect(()=>{
-   getData()
-},[])
+ 
+ 
 
 
   return (
@@ -60,7 +90,7 @@ useEffect(()=>{
      <Box ml={{base:"2px", sm: "20px", md: "20px", lg: "20px",xl: "20px",'2xl': "20px"}} >
      <Carousel responsive={responsive} > 
     
-     {data.length > 0 && data.map((item)=>{
+     {getData.length > 0 && getData.map((item)=>{
        return (
        <Image src={item.mainImage} alt={item.categories} boxSize={{base:"300px", sm: "300px", md: "300px", lg: "300px",xl: "300px",'2xl': "300px"}}  shadow="2xl" cursor="pointer" />
         
